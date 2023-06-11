8593d3222ff9536e33837ce8c06bc9ab95a0100d
richod <frimpsup@gmail.com>
2023-06-11 05:43:26 -0400

    endpint-init

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express=require("express")
 const app=express()
 const port=3000;
 const cors=require("cors")
-require('dotenv').congig()
-
+require('dotenv').config()
+const userRouter=require("./routes/user.Route")
+const bookRouter=require("./routes/book.Route")
+const dbConnection=require("./database/db.js")
 
 
 app.use(cors())
@@ -11,16 +13,21 @@ app.use(express.json())
 app.use(express.static('uploads'))
 
 
+// database connect
+dbConnection();
+
 // endpoint
 app.get("/api",(req,res)=>{
     res.send("api is open")
 })
 
 //user endpoint
+app.use("/user",userRouter)
 
 
 
 //book endpoint
+app.use("/book",bookRouter)
 
 //error point
 app.all('*',(req,res,next)=>{
